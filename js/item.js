f5675b2b8b859803f2c606e16acb7d53cc7c456b
ahmedelahwany <ahmed.elahwany26@gmail.com>
2021-06-24 17:12:36 +0200

    add js file which contains store items ( this can be changed based on preference)

---
+++ js/item.js
@@ -0,0 +1,76 @@
+var items =[
+        {
+            "id":"1",
+            "name":"cake item 1",
+             "img":"cake-1",
+             "price":"5"
+        },
+        {
+            "id":"2",
+            "name":"cake item 2",
+             "img":"cake-2",
+             "price":"10"
+        },
+        {
+            "id":"3",
+            "name":"cake item 3",
+             "img":"cake-3",
+             "price":"15"
+        },
+        {
+            "id":"4",
+            "name":"cupcake item 1",
+             "img":"cupcake-1",
+             "price":"5"
+        },
+        {
+            "id":"5",
+            "name":"cupcake item 2",
+             "img":"cupcake-2",
+             "price":"10"
+        },
+        {
+            "id":"6",
+            "name":"cupcake item 3",
+             "img":"cupcake-3",
+             "price":"15"
+        },
+        {
+            "id":"7",
+            "name":"doughnut item 1",
+             "img":"doughnut-1",
+             "price":"5"
+        },
+        {
+            "id":"8",
+            "name":"doughnut item 2",
+             "img":"doughnut-2",
+             "price":"10"
+        },
+        {
+            "id":"9",
+            "name":"doughnut item 3",
+             "img":"doughnut-3",
+             "price":"15"
+        },
+        {
+            "id":"10",
+            "name":"sweets item 1",
+             "img":"sweet-1",
+             "price":"5"
+        },
+        {
+            "id":"11",
+            "name":"sweets item 2",
+             "img":"sweet-2",
+             "price":"10"
+        },
+        {
+            "id":"12",
+            "name":"sweets item 3",
+             "img":"sweet-3",
+             "price":"15"
+        }
+        
+    ]
+    
